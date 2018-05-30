5e0f5f2e2fc1ba994ea55213ebe0620295760b91
Brendan Doyle <bdoyle522@gmail.com>
2018-05-30 11:32:45 -0500

    node running with babel, express server up, nodemon running from script

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from "./Components/Dashboard/main";
+import Form from "./Components/Form/main";
+import Header from "./Components/Header/main";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
+        <Dashboard />
+        <Form />
+        <Header />
+        
       </div>
     );
   }
---
+++ src/Components/Dashboard/main.jsx
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+import Product from "../Product/main";
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+      	Dashboard
+
+      	<Product />
+      	
+      </div>
+    );
+  }
+}
